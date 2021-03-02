15376924a6d7f9bf1c120f3b26cfe223ec9b81d3
Rachael Cox <rachaelcox@utexas.edu>
2021-03-02 14:41:52 -0600

    attempt #5 at col name matching bug

--- R/combine.R
+++ R/combine.R
@@ -132,11 +132,11 @@ combine_exps <- function(exp1, exp2, exp1_id, exp2_id, outfile_prefix){
                names(exp1), value = TRUE)
 
   df1 <- exp1 %>%
-    select(accession, joint_zscore, fdr_bh, get(acol)) %>%
+    select(accession, joint_zscore, fdr_bh, matches(".*gene_names_primary$")) %>%
     rename_with(.cols = all_of(cols), .fn = ~paste0(., exp1_id))
 
   df2 <- exp2 %>%
-    select(accession, joint_zscore, fdr_bh, get(acol)) %>%
+    select(accession, joint_zscore, fdr_bh, matches(".*gene_names_primary$")) %>%
     rename_with(.cols = all_of(cols), .fn = ~paste0(., exp2_id))
 
   df1_fdr <- colnames(df1)[3]
